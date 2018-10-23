5d53e083afa6659407c8ffaa016b06f0f9f46849
patzhang05 <44252571+patzhang05@users.noreply.github.com>
2018-10-23 10:15:42 -0700

    Template and Views

---
+++ alumni/directory/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+# Create your views here.
+from .models import 
+
+def index(request):
+
+def submit(request):
+
+def detail(request):
+
+def search(request):
+
+def statistics(request):
+
+def login(request):
+
+def approve(request):
+
