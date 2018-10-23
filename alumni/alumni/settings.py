c4a51be96a880ddb4cc46a615a181002c7d7294a
patzhang05 <44252571+patzhang05@users.noreply.github.com>
2018-10-23 10:20:36 -0700

    Test commit views and settings

--- alumni/alumni/settings.py
+++ alumni/alumni/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'directory.apps.DirectoryConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
--- alumni/directory/views.py
+++ alumni/directory/views.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 # Create your views here.
