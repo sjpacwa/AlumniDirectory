fe89a2f97f16feb5beeba7489af1af552247a674
sjpacwa <sjpacwa@gmail.com>
2018-10-23 10:21:31 -0700

    Fixed merge

--- alumni/directory/views.py
+++ alumni/directory/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 # Create your views here.
@@ -17,9 +16,3 @@ def statistics(request):
 def login(request):
 
 def approve(request):
-
-=======
-from django.shortcuts import render
-
-# Create your views here.
->>>>>>> 4f717d0bc074723fc2054bdcfc8e77629a4b22c2
