7088e60e89141fb8cecb3ae47ba92f5ff3210f3c
patzhang05 <44252571+patzhang05@users.noreply.github.com>
2018-11-27 00:23:50 -0800

    some basic unit testing with django

--- directory/tests.py
+++ directory/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import Business, Alumni
 
-# Create your tests here.
+class BusinessTestCase(TestCase):
+	def setUp(self):
+		Business.objects.create(business_name="Restauranttest123", 
+			business_address_one="1234", 
+			business_city="SCU", 
+			business_state="CA", 
+			business_zip="12345")
+
+	def test_business(self):
+		restaurant = Business.objects.get(business_name="Restauranttest123")
+		restaurant.assertEqual(restaurant.business_name=="Restauranttest123")
+		restaurant.assertEqual(restaurant.business_address_one=="1234")
+		restaurant.assertEqual(restaurant.business_city=="SCU")
+		restaurant.assertEqual(restaurant.business_state=="CA")
+		restaurant.assertEqual(restaurant.business_zip=="12345")
+
+class AlumniTestCase(TestCase):
+	def setUp(self):
+		Alumni.objects.create(alumni_first_name="Johntest123", 
+			alumni_last_name="Doe", 
+			alumni_major="COEN", 
+			alumni_grad="2019")
+
+	def test_alumni(self):
+		john = Alumni.objects.get(alumni_first_name="Johntest123")
+		john.assertEqual(john.alumni_first_name=="Johntest123")
+		john.assertEqual(john.alumni_last_name=="Doe")
+		john.assertEqual(john.alumni_major=="COEN")
+		john.assertEqual(john.alumni_grad=="2019")
